920fee6cba394a3e25c1b0048dfcf59aae8d7dbe
Pankaj <pankaj200635@gmail.com>
2021-09-23 13:18:18 +0530

    Testcase for Activity

---
+++ app/src/androidTest/java/com/example/kotlin_testdrivendevelopment/MainActivityTest.kt
@@ -0,0 +1,48 @@
+package com.example.kotlin_testdrivendevelopment
+
+import android.app.Activity
+import androidx.lifecycle.Lifecycle
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.launchActivity
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest{
+
+    private lateinit var scenario : ActivityScenario<Activity>
+
+    @Before
+    fun setup(){
+        scenario = launchActivity()
+        scenario.moveToState(Lifecycle.State.RESUMED)
+    }
+
+    @Test
+    fun testAndViewSpend(){
+        onView(withId(R.id.button_add_spend)).perform(click())
+        val amount = 100
+        val desc = "Bought Fruits"
+        //Espresso Matcher and Action
+        onView(withId(R.id.edit_text_amount)).perform(ViewActions.typeText(amount.toString()))
+        onView(withId(R.id.edit_text_description)).perform(ViewActions.typeText(desc))
+        Espresso.closeSoftKeyboard()
+        onView(withId(R.id.button_add)).perform(click())
+
+       onView(withText(amount.toString())).check(matches(isDisplayed()))
+       onView(withText(desc)).check(matches(isDisplayed()))
+
+    }
+}
